c24877d2084e555cb2aa3d26902ad8a8040a41ae
Hariom Mishra <mishraHariom855@gmail.com>
2026-08-24 19:43:15 +0530

    k8s pod implementation

--- sandbox/server/src/app.js
+++ sandbox/server/src/app.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
+import { createPod } from './kubernetes/pod.js';
+import { createService } from './kubernetes/service.js';
+import { v7 as uuid } from "uuid";
+
+console.log("APP.JS LOADED");
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-
+console.log("Registering start route");
 
 app.get('/api/sandbox/health', (req, res) => {
     res.status(200).json({
@@ -16,5 +20,31 @@ app.get('/api/sandbox/health', (req, res) => {
     });
 });
 
+app.post('/api/sandbox/start', async (req, res) => {
+    console.log("START ROUTE HIT");
+
+    const sandboxId = uuid();
+
+    await Promise.all([
+        createPod(sandboxId),
+        createService(sandboxId)
+    ]);
+
+    return res.status(200).json({
+        message: 'Sandbox created successfully',
+        sandboxId,
+        previewUrl: `https://${sandboxId}.localhost`,
+    });
+});
+
+console.log(
+    "Routes:",
+    app._router?.stack
+        ?.filter(layer => layer.route)
+        .map(layer => ({
+            path: layer.route.path,
+            methods: layer.route.methods
+        }))
+);
 
 export default app;
---
+++ sandbox/server/src/kubernetes/service.js
@@ -0,0 +1,35 @@
+import { k8sCoreV1Api } from "./config.js";
+
+export const createService = async (sandboxId) => {
+
+const serviceManifest = {
+        metadata: {
+            name: `sandbox-service-${sandboxId}`,
+            labels: {
+                sandboxId: sandboxId
+            }
+        },
+        spec: {
+            selector: {
+                sandboxId: sandboxId
+            },
+
+
+            ports: [
+                {
+                    name: "http",
+                    port: 80,
+                    targetPort: 5173,
+                    protocol: "TCP"
+                },
+                {
+                    name: "agent-http",
+                    port: 3000,
+                    targetPort: 3000,
+                    protocol: "TCP"
+                }
+            ],
+            type: "ClusterIP"
+        }
+    }  
+}
